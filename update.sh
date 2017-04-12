52d7e66ea4069f3ae23e4fe6416d1a2cc6061694
Clément Gillard <clement.gillard@epita.fr>
2017-04-12 17:56:20 +0200

    Add update script

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+dotfiles=$(dirname $0)
+
+files="bashrc gitconfig ssh/authorized_keys vimrc git-prompt.sh i3/config i3status.conf signature"
+
+git -C "$dotfiles" pull
+
+for f in $files
+do
+    target=~/."$f"
+    cmp "$dotfiles/$f" "$target" || vimdiff "$dotfiles/$f" "$target"
+    echo ".$f"
+done
