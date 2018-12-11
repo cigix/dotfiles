ce454a93e8044e2ee122bb6f1affb204f2853942
Clément Gillard <clement.gillard@epita.fr>
2018-12-11 14:10:52 +0100

    Update robust update.sh

--- update.sh
+++ update.sh
@@ -6,7 +6,15 @@ dotfiles=$(dirname $0)
 
 files="bashrc gitconfig ssh/authorized_keys vimrc git-prompt.sh i3/config i3status.conf profile signature utils/backlight-common.sh utils/backlight-down.sh utils/backlight-up.sh utils/screen-rotate-daemon.py ssh/config"
 
+old=$(md5sum "$0")
 git -C "$dotfiles" pull
+new=$(md5sum "$0")
+
+if [ "$old" != "$new" ]
+then
+    echo "$0 has been updated, please run it again" >&2
+    exit
+fi
 
 for f in $files
 do
